bc452f80b2d6b6c78a734a9f5428501e53b4114f
adaeze2022 <addyeduzo@gmail.com>
2022-06-25 09:26:48 +0100

    ggc

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * 
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+   printf("with ptoper grammar, but the outcome is a piece of art,\n");
+   return (0);
+}
