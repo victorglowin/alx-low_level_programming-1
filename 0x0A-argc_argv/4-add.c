99e6b2e2f7f108360689e79e21eed8cdd8e4c6ab
adaeze2022 <addyeduzo@gmail.com>
2022-07-22 13:17:45 +0100

    commit

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
 				if (isdigit(*(ptr + j)) == 0)
 				{
 					printf("Error\n");
-                    					return (1);
+					return (1);
 				}
 			}
 			sum += atoi(argv[i]);
